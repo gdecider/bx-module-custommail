53b021d585d2801238f96b8301b533855febabee
Denis M <gdecider@gmail.com>
2020-04-05 16:27:48 +0300

    Update include.php

--- include.php
+++ include.php
@@ -4,5 +4,5 @@ include __DIR__ . '/vendor/autoload.php';
 
 function custom_mail($to, $subject, $message, $additional_headers='', $additional_parameters='')
 {
-    \Local\Custommail\CustomMailer::getInstance()->send($to, $subject, $message, $additional_headers, $additional_parameters);
+    return \Local\Custommail\CustomMailer::getInstance()->send($to, $subject, $message, $additional_headers, $additional_parameters);
 }
